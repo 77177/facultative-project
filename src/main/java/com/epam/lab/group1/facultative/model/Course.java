7a139d83b7b909445edf89ffdfc0e22fb593a95b
aydin95777 <aliyev_aydin95@mail.ru>
2018-12-27 15:20:53 +0300

    Create models
    Create Student, Teacher, Course classes with lombok

--- src/main/java/com/epam/lab/group1/facultative/model/Course.java
+++ src/main/java/com/epam/lab/group1/facultative/model/Course.java
@@ -1,7 +1,12 @@
 package com.epam.lab.group1.facultative.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class Course {
     private int id;
     private String name;
@@ -9,43 +14,4 @@ public class Course {
     private Date startDate;
     private Date finalDate;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTeacherName() {
-        return teacherName;
-    }
-
-    public void setTeacherName(String teacherName) {
-        this.teacherName = teacherName;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getFinalDate() {
-        return finalDate;
-    }
-
-    public void setFinalDate(Date finalDate) {
-        this.finalDate = finalDate;
-    }
 }
--- src/main/java/com/epam/lab/group1/facultative/model/Student.java
+++ src/main/java/com/epam/lab/group1/facultative/model/Student.java
@@ -1,5 +1,10 @@
 package com.epam.lab.group1.facultative.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
     private int id;
     private String firstname;
@@ -7,43 +12,4 @@ public class Student {
     private String username;
     private String password;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
