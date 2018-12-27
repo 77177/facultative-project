4c5f1a1ea7d638dbf36d53deeed766853d37ad90
Stanislav Tarasov <iluvstrwrs@mail.ru>
2018-12-27 18:59:38 +0300

    Deprecated unneeded methods.

--- src/main/java/com/epam/lab/group1/facultative/controller/StudentController.java
+++ src/main/java/com/epam/lab/group1/facultative/controller/StudentController.java
@@ -19,44 +19,12 @@ public class StudentController {
     private final String viewName = "student";
 
     /**
-     * @return all student in db as list.
-     */
-    @GetMapping("")
-    @ResponseBody
-    public List<Student> getAll() {
-        return Collections.emptyList();
-    }
-
-    /**
      * @param studentId
      * @return Student by db's student's id with corresponding courseList.
      */
-    @GetMapping("/{studentId}")
+    @RequestMapping(value = "/{studentId}")
     public ModelAndView getById(@PathVariable int studentId) {
         ModelAndView modelAndView = new ModelAndView(viewName);
         return modelAndView;
-    }
-
-    /**
-     * @param request post-request from user.
-     * @return modelAndView with created by StudentService Student object with empty Course list.
-     */
-    @PostMapping("")
-    public ModelAndView create(HttpServletRequest request) {
-        ModelAndView modelAndView = new ModelAndView(viewName);
-        return modelAndView;
-    }
-
-    /**
-     * @param studentId student to delete
-     * @return redirect to /course if succeed, /student/studentId if not.
-     */
-    @DeleteMapping("")
-    public String delete(@PathVariable int studentId) {
-        if (false) {
-            return "redirect:/course";
-        } else {
-            return "redirect:/student/" + studentId;
-        }
     }
 }
