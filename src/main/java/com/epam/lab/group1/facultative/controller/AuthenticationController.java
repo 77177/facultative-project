211190b90b656337935ed4e92f2ed86ec26a0d7b
AleksandrEG <aleksandr.e.gusev@gmail.com>
2018-12-30 21:17:46 +0300

    Split registration method in two: get and post.
    Get part returns registration form page.
    Post method receive info from user and passes it to AuthenticationService.

--- src/main/java/com/epam/lab/group1/facultative/controller/AuthenticationController.java
+++ src/main/java/com/epam/lab/group1/facultative/controller/AuthenticationController.java
@@ -1,7 +1,10 @@
 package com.epam.lab.group1.facultative.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,6 +13,7 @@ public class AuthenticationController {
 
     private final String loginViewName = "login";
     private final String registrationViewName = "registration";
+    private final String courseViewName = "course";
 
     /**
      * @return official project login page.
@@ -21,11 +25,29 @@ public class AuthenticationController {
     }
 
     /**
-     * @return official project login page.
+     * @return page of registration form/
      */
-    @RequestMapping("/registration")
+    @GetMapping("/registration")
     public ModelAndView registration() {
+        boolean isAuthenticated = false;
         ModelAndView modelAndView = new ModelAndView(registrationViewName);
+        return modelAndView;
+    }
+
+    /**
+     * Receive information from user and passes it to AuthenticationService to process.
+     *
+     * @return page of all courses.
+     */
+    @PostMapping("/registration")
+    public ModelAndView registerNewUser(@RequestParam String firstName,
+                                        @RequestParam String lastName,
+                                        @RequestParam String email,
+                                        @RequestParam String password,
+                                        @RequestParam String position) {
+        //TODO redirect param-list to AuthenticationService. Which should create new UserDetails and save in DB.
+        //AuthenticationService creates new user, sets Authentication in SecurityContextHolder
+        ModelAndView modelAndView = new ModelAndView(courseViewName);
         return modelAndView;
     }
 }
