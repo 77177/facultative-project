cc5db585d635c368b3d620e74599e2055fc4ff95
Stanislav Tarasov <iluvstrwrs@mail.ru>
2019-01-06 15:22:03 +0300

    created student dao and the interface to it

---
+++ src/main/java/com/epam/lab/group1/facultative/daoImpl/StudentDAO.java
@@ -0,0 +1,34 @@
+package com.epam.lab.group1.facultative.daoImpl;
+
+import com.epam.lab.group1.facultative.daoInterface.DAO;
+import com.epam.lab.group1.facultative.model.Student;
+
+import java.util.List;
+
+public class StudentDAO implements DAO<Student> {
+
+    @Override
+    public Student get(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student deleteById(Student student) {
+        return null;
+    }
+
+    @Override
+    public List<Student> getList() {
+        return null;
+    }
+
+    @Override
+    public Student create(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student update(Student student) {
+        return null;
+    }
+}
---
+++ src/main/java/com/epam/lab/group1/facultative/daoInterface/DAO.java
@@ -0,0 +1,16 @@
+package com.epam.lab.group1.facultative.daoInterface;
+
+import java.util.List;
+
+public interface DAO<T> {
+
+    T get(T t);
+
+    T deleteById(T t);
+
+    List<T> getList();
+
+    T create(T t);
+
+    T update(T t);
+}
