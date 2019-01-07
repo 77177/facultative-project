1207f7ae478b20878b38cdc8288514f16e87595f
Stanislav Tarasov <iluvstrwrs@mail.ru>
2019-01-07 17:20:10 +0300

    Added Repository annotation

--- src/main/java/com/epam/lab/group1/facultative/daoImpl/StudentDAO.java
+++ src/main/java/com/epam/lab/group1/facultative/daoImpl/StudentDAO.java
@@ -3,11 +3,13 @@ package com.epam.lab.group1.facultative.daoImpl;
 import com.epam.lab.group1.facultative.daoInterface.DAO;
 import com.epam.lab.group1.facultative.model.Student;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.Collections;
 import java.util.List;
 
+@Repository
 public class StudentDAO implements DAO<Student> {
 
     private JdbcTemplate jdbcTemplate;
