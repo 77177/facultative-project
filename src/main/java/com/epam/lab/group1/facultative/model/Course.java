e2b4bfeb610e02a30f88a9caf2ff69d3c3d66d94
aydin95777 <aliyev_aydin95@mail.ru>
2019-01-09 14:33:20 +0300

    Update Course
    Date -> LocalDate

--- src/main/java/com/epam/lab/group1/facultative/model/Course.java
+++ src/main/java/com/epam/lab/group1/facultative/model/Course.java
@@ -2,7 +2,7 @@ package com.epam.lab.group1.facultative.model;
 
 import lombok.*;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -12,8 +12,8 @@ class Course {
     private int courseId;
     private String courseName;
     private String tutorId;
-    private Date startingDate;
-    private Date finishingDate;
+    private LocalDate startingDate;
+    private LocalDate finishingDate;
     private boolean active;
     private List<Student> studentsList;
 
