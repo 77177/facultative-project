fed98e7a5e34476793dea843b6636153063a4219
AleksandrEG <aleksandr.e.gusev@gmail.com>
2019-01-13 00:21:02 +0300

    wtf

--- src/main/java/com/epam/lab/group1/facultative/controller/WelcomeController.java
+++ src/main/java/com/epam/lab/group1/facultative/controller/WelcomeController.java
@@ -4,10 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@RequestMapping("/**")
 public class WelcomeController {
 
-    @GetMapping
     public ModelAndView welcome(){
         ModelAndView modelAndView = new ModelAndView("course");
         return modelAndView;
