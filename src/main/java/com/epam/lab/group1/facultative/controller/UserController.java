4d98c2ba7a568a34f9e9cbc36a342aa7c2d2fdc3
AleksandrEG <aleksandr.e.gusev@gmail.com>
2019-01-14 14:00:07 +0300

    UserController created

---
+++ src/main/java/com/epam/lab/group1/facultative/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.epam.lab.group1.facultative.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+}
