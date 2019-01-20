80b881af509523ba0832513bc117c4a1ef1c8429
AleksandrEG <aleksandr.e.gusev@gmail.com>
2019-01-21 02:29:20 +0300

    Added custom exceptions to FeedbackDao

--- src/main/java/com/epam/lab/group1/facultative/persistance/FeedBackDAO.java
+++ src/main/java/com/epam/lab/group1/facultative/persistance/FeedBackDAO.java
@@ -1,7 +1,9 @@
 package com.epam.lab.group1.facultative.persistance;
 
+import com.epam.lab.group1.facultative.exception.internal.PersistingEntityException;
 import com.epam.lab.group1.facultative.model.FeedBack;
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -20,20 +22,33 @@ public class FeedBackDAO {
     }
 
     public FeedBack getFeedBack(int courseId, int userId) {
-        Session session = sessionFactory.openSession();
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<FeedBack> query = criteriaBuilder.createQuery(FeedBack.class);
-        Root<FeedBack> root = query.from(FeedBack.class);
-        query.select(root).where(criteriaBuilder.equal(root.get("courseId"), courseId), criteriaBuilder.equal(root.get("studentId"), userId));
-        Query<FeedBack> queryFinal = session.createQuery(query);
-        FeedBack feedBack = queryFinal.getSingleResult();
-        return feedBack;
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<FeedBack> query = criteriaBuilder.createQuery(FeedBack.class);
+            Root<FeedBack> root = query.from(FeedBack.class);
+            query.select(root).where(criteriaBuilder.equal(root.get("courseId"), courseId), criteriaBuilder.equal(root.get("studentId"), userId));
+            Query<FeedBack> queryFinal = session.createQuery(query);
+            FeedBack feedBack = queryFinal.getSingleResult();
+            session.getTransaction().commit();
+            return feedBack;
+        } catch (HibernateException e) {
+            String error = String .format("Error during retrieving feedBack by courseId: %d and userId: %d", courseId
+                , userId);
+            logger.error(error);
+            throw new PersistingEntityException(error, e);
+        }
     }
 
     public void saveOrUpdate(FeedBack feedBack) {
-        Session session = sessionFactory.openSession();
-        session.getTransaction().begin();
-        session.saveOrUpdate(feedBack);
-        session.getTransaction().commit();
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.saveOrUpdate(feedBack);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            String error = "Error during feedBack persisting. " + feedBack;
+            logger.error(error);
+            throw new PersistingEntityException(error, e);
+        }
     }
 }
