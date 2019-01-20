2338099b48665563c8a832bca4f2219e07de05d0
AleksandrEG <aleksandr.e.gusev@gmail.com>
2019-01-21 01:41:22 +0300

    Implemented exception classes.

--- src/main/java/com/epam/lab/group1/facultative/exception/internal/UserWithEmailDoesNotExistException.java
+++ src/main/java/com/epam/lab/group1/facultative/exception/internal/UserWithEmailDoesNotExistException.java
@@ -1,4 +1,23 @@
 package com.epam.lab.group1.facultative.exception.internal;
 
-public class UserWithEmailDoesNotExistException {
+public class UserWithEmailDoesNotExistException extends EntityNotFoundException {
+
+    public UserWithEmailDoesNotExistException() {
+    }
+
+    public UserWithEmailDoesNotExistException(String message) {
+        super(message);
+    }
+
+    public UserWithEmailDoesNotExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserWithEmailDoesNotExistException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserWithEmailDoesNotExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
