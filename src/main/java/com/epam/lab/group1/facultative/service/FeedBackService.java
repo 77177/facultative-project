b27bc58161f4c24ebedb0b7e29229c4a2cb2832e
Stanislav Tarasov <iluvstrwrs@mail.ru>
2019-01-28 17:59:48 +0300

    fixed check for eligibility of viewing the feedbacks

--- src/main/java/com/epam/lab/group1/facultative/service/FeedBackService.java
+++ src/main/java/com/epam/lab/group1/facultative/service/FeedBackService.java
@@ -3,24 +3,28 @@ package com.epam.lab.group1.facultative.service;
 import com.epam.lab.group1.facultative.exception.NotTheCourseTutorException;
 import com.epam.lab.group1.facultative.model.Course;
 import com.epam.lab.group1.facultative.model.FeedBack;
+import com.epam.lab.group1.facultative.model.User;
 import com.epam.lab.group1.facultative.persistance.FeedBackDAO;
 import com.epam.lab.group1.facultative.persistance.FeedBackDAOInterface;
 import com.epam.lab.group1.facultative.security.SecurityContextUser;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FeedBackService implements FeedBackServiceInterface {
 
     private FeedBackDAOInterface feedBackDAO;
     private CourseServiceInterface courseService;
+    private UserServiceInterface userService;
 
-    public FeedBackService(FeedBackDAO feedBackDAO, CourseService courseService) {
+    public FeedBackService(FeedBackDAO feedBackDAO, CourseService courseService,UserService userService) {
         this.feedBackDAO = feedBackDAO;
         this.courseService = courseService;
+        this.userService = userService;
     }
 
     @Override
@@ -28,7 +32,9 @@ public class FeedBackService implements FeedBackServiceInterface {
 
         SecurityContextUser principal = (SecurityContextUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Course byId = courseService.getById(courseId);
-        if (principal.getUserId() == byId.getTutorId()) {
+        List<Integer> allStudentByCourseId = userService.getAllStudentByCourseId(courseId).stream().map(x->x.getId()).collect(Collectors.toList());
+
+        if (principal.getUserId() == byId.getTutorId() || allStudentByCourseId.contains(userId)) {
             return feedBackDAO.getFeedBack(courseId, userId);
         } else {
             throw new NotTheCourseTutorException("you are not eligible to view this info");
@@ -40,7 +46,8 @@ public class FeedBackService implements FeedBackServiceInterface {
     public void saveOrUpdateFeedBack(FeedBack feedBack) {
         SecurityContextUser principal = (SecurityContextUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Course byId = courseService.getById(feedBack.getCourseId());
-        if (principal.getUserId() == byId.getTutorId()) {
+        List<Integer> allStudentByCourseId = userService.getAllStudentByCourseId(feedBack.getCourseId()).stream().map(x->x.getId()).collect(Collectors.toList());
+        if (principal.getUserId() == byId.getTutorId() || allStudentByCourseId.contains(feedBack.getStudentId())) {
             feedBackDAO.saveOrUpdateFeedBack(feedBack);
         } else {
             throw new NotTheCourseTutorException("you are not eligible to view this info");
